3d860d04b5aff652d63e1d8afd07fcba3a67ac60
오유찬 <oyuchan50@gmail.com>
2023-04-18 12:24:48 +0900

    :sparkles:페이징 처리 #2

--- Book/views.py
+++ Book/views.py
@@ -2,8 +2,13 @@ from rest_framework import generics
 from Book.serializers import BookSerializer
 from .models import BOOK
 from rest_framework.permissions import AllowAny
+from rest_framework.pagination import PageNumberPagination
+
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size=20
 
 class BookList(generics.ListAPIView):
     queryset = BOOK.objects.all()
     serializer_class = BookSerializer
     permission_classes = [AllowAny]
+    pagination_class = PageNumberPagination
