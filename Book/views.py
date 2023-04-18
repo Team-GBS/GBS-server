f7c6640f51b24033b5706915dd9c02fffde68a29
오유찬 <oyuchan50@gmail.com>
2023-04-18 13:52:38 +0900

    :sparkles: 페이징 처리 def로 변경하기 #2 - 20까지 페이징 처리 완료

--- Book/views.py
+++ Book/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+# from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from Book.serializers import BookSerializer
 from .models import BOOK
 from rest_framework.permissions import AllowAny
@@ -7,8 +9,23 @@ from rest_framework.pagination import PageNumberPagination
 class CustomPageNumberPagination(PageNumberPagination):
     page_size=20
 
-class BookList(generics.ListAPIView):
-    queryset = BOOK.objects.all()
-    serializer_class = BookSerializer
+# class BookList(generics.ListAPIView):
+#     queryset = BOOK.objects.all()
+#     serializer_class = BookSerializer
+#     permission_classes = [AllowAny]
+#     pagination_class = PageNumberPagination
+
+
+class BookList(APIView):
     permission_classes = [AllowAny]
-    pagination_class = PageNumberPagination
+    pagination_class = CustomPageNumberPagination
+
+    def get(self, request):
+        books = BOOK.objects.all()
+        paginator = self.pagination_class()
+        page = paginator.paginate_queryset(books, request)
+        if page is not None:
+            serializer = BookSerializer(page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
