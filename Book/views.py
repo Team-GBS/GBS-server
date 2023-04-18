f7a5e912d67104907ee675aee93c00ffc63a8d78
오유찬 <oyuchan50@gmail.com>
2023-04-18 14:22:04 +0900

    :sparkles: 페이지가 몇 페이지인지 확인하는 코드 추가 - /books/?page=n 을 하면 20권의 책이 나눠져 있는 n번째 page로 이동

--- Book/views.py
+++ Book/views.py
@@ -1,7 +1,8 @@
 # from rest_framework import generics
+from django.http import HttpRequest
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from Book.serializers import BookSerializer
+from .serializers import BookSerializer
 from .models import BOOK
 from rest_framework.permissions import AllowAny
 from rest_framework.pagination import PageNumberPagination
@@ -9,6 +10,7 @@ from rest_framework.pagination import PageNumberPagination
 class CustomPageNumberPagination(PageNumberPagination):
     page_size=20
 
+# 이전 코드
 # class BookList(generics.ListAPIView):
 #     queryset = BOOK.objects.all()
 #     serializer_class = BookSerializer
@@ -16,16 +18,18 @@ class CustomPageNumberPagination(PageNumberPagination):
 #     pagination_class = PageNumberPagination
 
 
+# def로 변환
 class BookList(APIView):
     permission_classes = [AllowAny]
     pagination_class = CustomPageNumberPagination
 
-    def get(self, request):
+    def get(self, request:HttpRequest)->Response:
         books = BOOK.objects.all()
         paginator = self.pagination_class()
-        page = paginator.paginate_queryset(books, request)
+        page = request.GET.get('page')
         if page is not None:
-            serializer = BookSerializer(page, many=True)
+            page_books = paginator.paginate_queryset(books, request)
+            serializer = BookSerializer(page_books, many=True)
             return paginator.get_paginated_response(serializer.data)
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
