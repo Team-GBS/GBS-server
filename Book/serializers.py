fd1b07c78aef81046db2b5d87d93b9ed32bc5081
오유찬 <oyuchan50@gmail.com>
2023-04-20 17:33:25 +0900

    :bulb: Paging 기능 주석 달기 - 아직 개발이 미숙하여 잘 기억나지 않은 모듈들이 있어 구현한 기능을   자세히 주석을 달았습니다.

--- Book/serializers.py
+++ Book/serializers.py
@@ -3,5 +3,8 @@ from .models import BOOK
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
+        # BookSerializer가 사용할 모델을 지정합니다.
         model = BOOK
+        
+        # 직렬화할 필드를 모두 지정합니다.
         fields = '__all__'
